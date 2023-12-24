aeda59fe4cb1cf9401b81d0377651b16f4aadbf6
Parimala Vaddem <parimala.vaddem-ext@tbccorp.com>
2023-12-24 22:33:21 +0530

    updating script with webdriver manager

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,6 +1,6 @@
 package com.qascript;
 
-//import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 //import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
@@ -15,7 +15,7 @@ public class FirstTest {
     @Test
     public void OpenBrowser()  {
         WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+       // System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
 //        options.addArguments("disable-gpu");
@@ -24,7 +24,7 @@ public class FirstTest {
         options.addArguments("--disable-dev-shm-usage");
 
 
-       // WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
         //maximize window
          System.out.println("maximize window");
